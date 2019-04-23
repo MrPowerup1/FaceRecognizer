ad3731a9e208d8f1c3404029f25df5eb61d3378b
Jonathan Bovee <jbovee99@gmail.com>
2019-04-23 11:27:56 -0500

    initial commit

--- app/src/main/java/com/example/facerecognizer/MainActivity.java
+++ app/src/main/java/com/example/facerecognizer/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.facerecognizer;
 
+import android.hardware.Camera;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * the id of the camera being Used.
+     */
+    private static final int CAMERA_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ImageView image = findViewById(R.id.imageView2);
+        image.setVisibility(View.VISIBLE);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Camera.open(CAMERA_ID);
+        SurfaceHolder forCam = new SurfaceView();
+
     }
 
     @Override
